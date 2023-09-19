0515493e664621f7054827e515bcdd8eea1713f5
fabien-ricca <fabien.ricca@laplateforme.io>
2023-09-19 11:11:31 +0200

    Jour2_job02

--- Jour2/job01_&_02/demo/src/main/java/com/example/demo/controller/ViewController.java
+++ Jour2/job01_&_02/demo/src/main/java/com/example/demo/controller/ViewController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-
-    public String message = "Bonjour";
+    public static String messageIndex = "Bienvenue sur l'index.";
+    public static String message2 = "Bienvenue sur la page 2.";
+    public static String message3 = "Bienvenue sur la page 3.";
 
     @GetMapping("/")
-    public String view(Model model){
-        model.addAttribute("message", msg());
-        return "view";
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/page2")
+    public String page2(){
+        return "page2";
+    }
+
+    @GetMapping("/page3")
+    public String page3(){
+        return "page3";
     }
 
     public static String msg(){
